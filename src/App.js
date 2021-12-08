cee8f800bf4844cb5861ff446c53724cf57735c2
dhruvildave22 <dhruvildave22@gmail.com>
2021-12-08 10:48:23 +0530

    get env variable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import Card from './components/Card';
+import React from "react";
+import Card from "./components/Card";
 
 function App() {
-  return <Card />;
+  return (
+    <>
+      <Card />
+      <div>App.js {process.env.REACT_APP_DT_CLIENT_ID}</div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function Card() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "World";
+  return (
+    <>
+      <div>Hello {value}</div>
+      <div>Card.js {process.env.REACT_APP_DT_CLIENT_ID}</div>
+    </>
+  );
 }
 
 export default Card;
